7a26e46c29b1bd6b2cabb83734808fb1b0fee1d1
ibrahim mahmoud elghandour <ibrahimelghandour51@gmail.com>
2026-09-19 23:00:01 +0300

    Add exam results and grade calculation

--- ExamSystemRoute/Exams/FinalExam.cs
+++ ExamSystemRoute/Exams/FinalExam.cs
@@ -21,10 +21,17 @@ namespace ExamSystem.Exams
 
         public override void ShowExam()
         {
-            Console.WriteLine(" Final Exam");
+            int grade = 0;
+            int totalMarks = 0;
+
+            Answer[] userAnswers = new Answer[Questions.Length];
+
+            Console.WriteLine("Final Exam");
             Console.WriteLine($"Exam Time: {Time} Minutes");
             Console.WriteLine($"Number of Questions: {NumberOfQuestions}");
             Console.WriteLine();
+
+            #region Show Questions And Get Answers
 
             for (int i = 0; i < Questions.Length; i++)
             {
@@ -36,8 +43,46 @@ namespace ExamSystem.Exams
                     Console.WriteLine(answer);
                 }
 
+                Console.Write("Your Answer: ");
+
+                int userAnswerId;
+
+                while (!int.TryParse(Console.ReadLine(), out userAnswerId)
+                       || userAnswerId < 1
+                       || userAnswerId > Questions[i].AnswerList.Length)
+                {
+                    Console.Write("Invalid Answer, Enter Again: ");
+                }
+
+                userAnswers[i] = Questions[i].AnswerList[userAnswerId - 1];
+
+                totalMarks += Questions[i].Mark;
+
+                if (userAnswers[i].AnswerId == Questions[i].RightAnswer.AnswerId)
+                {
+                    grade += Questions[i].Mark;
+                }
+
                 Console.WriteLine();
             }
+
+            #endregion
+
+            #region Show Final Result
+
+            Console.WriteLine("Exam Result");
+            Console.WriteLine();
+
+            for (int i = 0; i < Questions.Length; i++)
+            {
+                Console.WriteLine($"Question {i + 1}: {Questions[i].Body}");
+                Console.WriteLine($"Your Answer: {userAnswers[i]}");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine($"Grade: {grade} / {totalMarks}");
+
+            #endregion
         }
 
         #endregion
--- ExamSystemRoute/Exams/PracticalExam.cs
+++ ExamSystemRoute/Exams/PracticalExam.cs
@@ -21,11 +21,11 @@ namespace ExamSystem.Exams
 
         public override void ShowExam()
         {
-            Console.WriteLine(" Practical Exam ");
+            Console.WriteLine("Practical Exam");
             Console.WriteLine($"Exam Time: {Time} Minutes");
             Console.WriteLine($"Number of Questions: {NumberOfQuestions}");
             Console.WriteLine();
-                
+
             for (int i = 0; i < Questions.Length; i++)
             {
                 Console.WriteLine($"Question {i + 1}");
@@ -36,7 +36,26 @@ namespace ExamSystem.Exams
                     Console.WriteLine(answer);
                 }
 
+                Console.Write("Your Answer: ");
+                int userAnswer;
+
+                while (!int.TryParse(Console.ReadLine(), out userAnswer)
+                       || userAnswer < 1
+                       || userAnswer > Questions[i].AnswerList.Length)
+                {
+                    Console.Write("Invalid Answer, Enter Again: ");
+                }
+
                 Console.WriteLine();
+            }
+
+            Console.WriteLine("Right Answers:");
+
+            for (int i = 0; i < Questions.Length; i++)
+            {
+                Console.WriteLine(
+                    $"Question {i + 1}: {Questions[i].RightAnswer}"
+                );
             }
         }
 
